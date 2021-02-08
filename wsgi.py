f2ca4b6cf6859cd1af7cb4fe9d9c68f2ef1a174e
김대영 <philip@gimdaeyeong-ui-MacBookPro.local>
2021-02-09 01:34:42 +0900

    Feat: ssl context is added

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
 from app import app
 
 if __name__ == '__main__':
-    app.run(port=5000, host="0.0.0.0", debug=False)
+    app.run(port=5000, host="0.0.0.0", debug=False,
+            ssl_context=('/etc/letsencrypt/live/i-api.co.kr/fullchain.pem',
+                         '/etc/letsencrypt/live/i-api.co.kr/privkey.pem'))
