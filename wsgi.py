ba47c1a8f7d6770f4b017f3b92953378f9faa75e
Ubuntu <ubuntu@ip-172-31-40-219.ap-northeast-2.compute.internal>
2021-02-09 00:34:17 +0000

    Fix: uwsgi.py Fixed

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,11 @@
-from app import app
+#from app import app
 
-if __name__ == '__main__':
-    app.run(port=5000, host="0.0.0.0", debug=False,
-            ssl_context=('/etc/letsencrypt/live/i-api.co.kr/fullchain.pem',
-                         '/etc/letsencrypt/live/i-api.co.kr/privkey.pem'))
+#if __name__ == '__main__':
+#    app.run(port=5000, host="0.0.0.0", debug=False,
+#            ssl_context=('/etc/letsencrypt/live/i-api.co.kr/fullchain.pem',
+#                         '/etc/letsencrypt/live/i-api.co.kr/privkey.pem'))
+
+from app import app 
+
+if __name__ == "__main__": 
+    app.run()
