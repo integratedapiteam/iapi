82ea01fea71b6939581a31c0ad4fc1bbeb85f707
김대영 <philip@gimdaeyeong-ui-MacBookPro.local>
2021-02-09 14:55:13 +0900

    Feat: ssl is deleted

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,4 @@
 from app import app
 
 if __name__ == '__main__':
-    app.run(port=5000, host="0.0.0.0", debug=False,
-            ssl_context=('/etc/letsencrypt/live/i-api.co.kr/fullchain.pem',
-                         '/etc/letsencrypt/live/i-api.co.kr/privkey.pem'))
+    app.run(port=5000, host="0.0.0.0")
