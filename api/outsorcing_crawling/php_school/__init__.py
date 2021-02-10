b44694cb9a846a4d8023a12e47e7694557d2e878
김대영 <philip@gimdaeyeong-ui-MacBookPro.local>
2021-02-10 13:44:19 +0900

    Feat: db connection

--- api/outsorcing_crawling/php_school/__init__.py
+++ api/outsorcing_crawling/php_school/__init__.py
@@ -16,14 +16,27 @@ def get_outsourcing_info():
     try:
         result_html = requests.get("https://www.phpschool.com/gnuboard4/bbs/board.php?bo_table=old_job&page=1")
         soup = bs4.BeautifulSoup(result_html.text, "html.parser")
+        message = "새 외주가 없습니다."
 
-        db.session.add(CrawlingLastPage(FilterHTML.filter_html(soup.select("td[class*='subject']")[3].find("span"), {}),
-                                           "php_school"))
+        last_page = CrawlingLastPage.query.filter_by(last_content_title=FilterHTML.filter_html(soup.select("td[class*='subject']")[3].find("span"), {}))
 
-        message = "PHP SCHOOL에서 새 외주가 있습니다. 제목: " + FilterHTML.filter_html(soup.select("td[class*='subject']")[3]
-                                                                           .find("span"), {})
+        if last_page is None:
+            db.session.add(
+                CrawlingLastPage(FilterHTML.filter_html(soup.select("td[class*='subject']")[3].find("span"), {}),
+                                 "php_school"))
+            db.session.commit()
 
-        send_sms_message(message, "01057949511,01056046071")
+            message = "PHP SCHOOL에서 새 외주가 있습니다. 제목: " + FilterHTML.filter_html(soup.select("td[class*='subject']")[3]
+                                                                               .find("span"), {})
+
+            send_sms_message(message, "01057949511,01056046071")
+        else:
+            if last_page.last_content_title != FilterHTML.filter_html(soup.select("td[class*='subject']")[3]
+                                                                               .find("span"), {}):
+                db.session.delete(CrawlingLastPage(FilterHTML.filter_html(soup.select("td[class*='subject']")[3].find("span"), {}),
+                                 "php_school"))
+                db.session.commit()
+
         result = {"message": "success", "return": message}
         return jsonify(result), 200
     except Exception as e:
