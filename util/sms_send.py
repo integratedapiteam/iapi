8537e3653540e8c6d8b924beebaef9404c0ea7a3
김대영 <philip@gimdaeyeong-ui-MacBookPro.local>
2021-02-10 13:01:30 +0900

    Feat: sender is added

--- util/sms_send.py
+++ util/sms_send.py
@@ -5,11 +5,13 @@ from config import ALIGO_KEY, ALIGO_IDENTIFIER
 
 def send_sms_message(message, receiver):
     try:
-        requests.post("https://apis.aligo.in/send", {
+        result = requests.post("https://apis.aligo.in/send", {
             "key": ALIGO_KEY,
             "user_id": ALIGO_IDENTIFIER,
             "message": message,
-            "receiver": receiver
+            "receiver": receiver,
+            "sender": "01057949511"
         })
+
     except Exception as e:
         logger.error(e, exc_info=True)
