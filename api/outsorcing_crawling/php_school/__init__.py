c654db64a56f3a44f0895e8eae3f4e83a67c0cf2
김대영 <philip@gimdaeyeong-ui-MacBookPro.local>
2021-02-10 12:30:38 +0900

    Feat: phpschool

--- api/outsorcing_crawling/php_school/__init__.py
+++ api/outsorcing_crawling/php_school/__init__.py
@@ -4,12 +4,18 @@ import FilterHTML
 
 from app import db
 from models.CrawlingLastPage import CrawlingLastPage
+from util.sms_send import send_sms_message
 
 result_html = requests.get("https://www.phpschool.com/gnuboard4/bbs/board.php?bo_table=old_job&page=1")
 soup = bs4.BeautifulSoup(result_html.text, "html.parser")
 
 db.session.create(CrawlingLastPage(FilterHTML.filter_html(soup.select("td[class*='subject']")[3].find("span"), {}),
                                    "php_school"))
+
+message = "PHP SCHOOL에서 새 외주가 있습니다. 제목: " + FilterHTML.filter_html(soup.select("td[class*='subject']")[3]
+                                                                   .find("span"), {})
+
+send_sms_message(message, "01057949511,01056046071")
 
 
 
