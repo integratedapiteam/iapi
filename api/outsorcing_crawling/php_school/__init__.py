1a05c1ba06d89c533b121478ae9aa60dcaa90d9c
김대영 <philip@gimdaeyeong-ui-MacBookPro.local>
2021-02-10 12:35:58 +0900

    Feat: php school

--- api/outsorcing_crawling/php_school/__init__.py
+++ api/outsorcing_crawling/php_school/__init__.py
@@ -2,14 +2,20 @@ import requests
 import bs4
 import FilterHTML
 
-from app import db
+from config import SQLALCHEMY_DATABASE_URI
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
 from models.CrawlingLastPage import CrawlingLastPage
 from util.sms_send import send_sms_message
+
+engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URI)
+Session = sessionmaker(bind=engine)
+session = Session()
 
 result_html = requests.get("https://www.phpschool.com/gnuboard4/bbs/board.php?bo_table=old_job&page=1")
 soup = bs4.BeautifulSoup(result_html.text, "html.parser")
 
-db.session.create(CrawlingLastPage(FilterHTML.filter_html(soup.select("td[class*='subject']")[3].find("span"), {}),
+session.add(CrawlingLastPage(FilterHTML.filter_html(soup.select("td[class*='subject']")[3].find("span"), {}),
                                    "php_school"))
 
 message = "PHP SCHOOL에서 새 외주가 있습니다. 제목: " + FilterHTML.filter_html(soup.select("td[class*='subject']")[3]
